3f2e4f4ebf7c489ae9e46ff47a9fe1fc0d7a90a5
DESKTOP-3RJ3712\GRIDA <einsoni@naver.com>
2024-07-04 14:35:15 +0900

    no message

--- static/js/plf_pub.js
+++ static/js/plf_pub.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
     tabSwitcher('my-tab-switching .tab .item'); //탭
     subCalendar(); //심의일정
     datepickerKr();
+    $(".datepicker1").length && datepicker();
     foldertree(); //평가표관리
     groupmanage() //그룹관리
     hcLayer.init(); //레이어팝업
@@ -73,6 +74,14 @@ function accordionCheckbox(){
             $(this).next().slideDown();
         }else{
             $(this).next().slideUp();
+        }
+    });
+    $(".btn-acco").click(function(){
+        $(this).parent().parent().toggleClass('open');
+        if($(this).parent().parent().hasClass('open')){
+            $(this).parent().next().slideDown();
+        }else{
+            $(this).parent().next().slideUp();
         }
     });
 }
@@ -185,6 +194,61 @@ function datepickerKr() {
         language: 'ko-KR',
         format: 'yyyy-mm-dd'
     });
+}
+
+function datepicker() {
+    // mark 날짜
+	var reqularDates = ["2024-06-11", "2024-05-15", "2024-06-13"]; // 정규심의 날짜
+	var fastDates = ["2024-06-11", "2024-06-04", "2024-06-05"]; // 신속심의 날짜
+
+	function available(date) {
+
+		var thismonth = date.getMonth()+1;
+		var thisday = date.getDate();
+
+		if(thismonth<10){
+			thismonth = "0"+thismonth;
+		}
+
+		if(thisday<10){
+			thisday = "0"+thisday;
+		}
+
+		ymd = date.getFullYear() + "-" + thismonth + "-" + thisday;
+
+        if ($.inArray(ymd, reqularDates) >= 0 && $.inArray(ymd, fastDates) >= 0) {
+			return [true,"reqular fast"];
+		} else if ($.inArray(ymd, reqularDates) >= 0) {
+			return [true,"reqular"];
+		}  else if ($.inArray(ymd, fastDates) >= 0) {
+			return [true,"fast"];
+		} else {
+			return [true,""];
+		}
+ 	}
+
+	$.datepicker.setDefaults({
+		dateFormat: 'yy-mm-dd',
+		//	minDate: '-7',	// 일주일 이전 날짜 선택 못하게
+		//	maxDate: '1',	// 하루 이후 날짜 선택 못하게
+
+		beforeShowDay: available,	// mark 표시
+
+		prevText: '이전 달',
+		nextText: '다음 달',
+		monthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+		monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+		dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		showMonthAfterYear: true,
+		yearSuffix: '.',
+
+		showOn: 'both', //	button,both : "button" ->button 클릭 시 날짜 위젯을 보여준다. "both" => button이나 text상자 클릭 시 날짜 위젯을 보여준다.
+		buttonImage: '../static/img/icon/ic_pagi.svg',  // 우측 달력 icon 의 이미지 패스
+		buttonImageOnly: true, // true이면 입력 필드 옆에 이미지만 표시됩니다. false이면 버튼 안에 이미지가 표시됩니다. inputbox 뒤에 달력icon만 표시한다. ('...' 표시생략)
+	});
+    $(".datepicker1").datepicker();
 }
 
 function foldertree() {
